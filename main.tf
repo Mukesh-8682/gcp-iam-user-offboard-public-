3cc3a610cab545c2fad2979e55d7df6a02cbe30f
mukesh <mukesh.muthukumar@ankercloud.com>
2026-05-15 20:43:30 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+locals {
+  roles_to_remove = [] 
+}
+
 resource "google_project_iam_member" "user_access" {
+  for_each = toset(local.roles_to_remove)
+
   project = var.project_id
-  role    = var.role
   member  = var.member
+  role    = each.value   
 }
